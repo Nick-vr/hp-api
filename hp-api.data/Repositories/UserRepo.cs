f9bc96e11b8f9fcc72a350329d015fe4cc11d778
Nick-vr <nvanroyen@gmail.com>
2021-05-27 22:57:04 +0200

    .

--- hp-api.data/Repositories/UserRepo.cs
+++ hp-api.data/Repositories/UserRepo.cs
@@ -5,6 +5,26 @@
         public UserRepo()
         {
             
+            // public async Task<IEnumerable<User>> FindAllAsync()
+            // {
+            //     var sql = $@"SELECT * FROM {UserSchema.Table}";
+            //     using var dbConnection = _dbConnection;
+            //     return await dbConnection.QueryAsync<User>(sql);
+            // }
+            //
+            // public async Task<User> FindByIdAsync(int id)
+            // {
+            //     var sql = $@"SELECT * FROM {UserSchema.Table} WHERE {UserSchema.Columns.Id} = @{nameof(id)}";
+            //     using var dbConnection = _dbConnection;
+            //     return await dbConnection.QueryFirstOrDefaultAsync<User>(sql, new { id });
+            // }
+            //
+            // public async Task<User> FindByUserNameAsync(string username)
+            // {
+            //     var sql = $@"SELECT * FROM  {UserSchema.Table} WHERE {UserSchema.Columns.UserName} = @{nameof(username)}";
+            //     using var dbConnection = _dbConnection;
+            //     return await dbConnection.QuerySingleOrDefaultAsync<User>(sql, new { username });
+            // }
         }
     }
 }
