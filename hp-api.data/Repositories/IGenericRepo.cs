6cefaac38a3e45c71e67adab14a2dfa377213b37
Nick-vr <nvanroyen@gmail.com>
2021-05-31 13:11:57 +0200

    generic interface

--- hp-api.data/Repositories/IGenericRepo.cs
+++ hp-api.data/Repositories/IGenericRepo.cs
@@ -1,6 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using hp_api.data.Models;
+
 namespace hp_api.data.Repositories
 {
-    public interface IGenericRepo<T>
+    public interface IGenericRepo<T> where T : BaseModel
     {
+        Task<IEnumerable<T>> GetAllAsync(string sql);
+        Task<T> GetByIdAsync(int id, string sql);
     }
 }
